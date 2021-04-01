533443cd2fcfb9e86f312023f563e1c1de92cc8f
AishwaryaZope <vmahajan2009@gmail.com>
2021-04-01 10:48:40 +0530

    Practical No 1

---
+++ pages/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse 
+
+# Create your views here.
+def homePageView(request):
+	return HttpResponse('Hello,This is Aishwarya Zope from T4 Batch and my PRN is 1942201')
